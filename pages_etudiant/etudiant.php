9f9d04c6024bebeec27f26109bce5b30f116cea1
Louis <louis.houzet@isen.yncrea.fr>
2021-01-25 16:36:50 +0100

    Maj étudiant.php

--- pages_etudiant/etudiant.php
+++ pages_etudiant/etudiant.php
@@ -1,1 +1,11 @@
-<?php require_once('includes/header.php'); ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page_étudiant</title>
+</head>
+<body>
+    <?php require_once('includes/header.php'); ?>
+</body>
+</html>
