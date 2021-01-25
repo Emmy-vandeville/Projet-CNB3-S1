f75e80f55d95d04b87e32e342fcd5c910d548f4e
Emmy Vandeville <emmy.v2000@outlook.fr>
2021-01-25 17:10:34 +0100

    CSS et include header page etudiant

--- pages_etudiant/etudiant.php
+++ pages_etudiant/etudiant.php
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Page_étudiant</title>
-</head>
-<body>
-    <?php require_once('includes/header.php'); ?>
-</body>
-</html>
+<?php require_once('../includes/header.php'); ?>
+
+<main>
+</main>
