305b786ef8b48cba911f86c0e0090b78edb1dcf6
charlotte <balzac.charlotte@gmail.com>
2021-01-26 19:14:37 +0100

    supprimer fonctionnel

--- config/creation_grp.php
+++ config/creation_grp.php
@@ -9,12 +9,12 @@ if (isset($_POST['ajout'])){
   {
       // Initialisation des caractères utilisables
       $characters = array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
-  
+
       for($j=0;$j<$size;$j++)
       {
           $password .= ($j%2) ? strtoupper($characters[array_rand($characters)]) : $characters[array_rand($characters)];
       }
-          
+
       return $password;
   }
 
--- pages_etudiant/supprimer.php
+++ pages_etudiant/supprimer.php
@@ -1,12 +1,18 @@
 <?php
+require_once('../config/configdb.php');
+
 if (isset($_POST['id'])) {
   $ids = json_decode($_POST['id']);
   //$ids c'est le tableau JS en php
-  var_dump($ids);
+
+    //requete de préparatiàon : je dis ce que je vais faire
+    $requete = $conn->prepare('DELETE FROM photo WHERE id_photo = ?');
 
   foreach ($ids as $id) {
     //requête SQL
-    DELETE FROM 'table' WHERE 'id' = 'ids' // a modifier quand on aura la BDD
+    $requete->execute(array($id));
   }
 }
+header('Location: mes_photos.php');
+exit();
 ?>
