965ddd214e5af477c8baeb413288fd5aa7f2c0a2
Emmy Vandeville <emmy.v2000@outlook.fr>
2021-01-26 09:06:54 +0100

    Nv grp

--- pages_enseignant/nouveau_grp.php
+++ pages_enseignant/nouveau_grp.php
@@ -1,3 +1,27 @@
+<?php /*      //   A REVOIR METHODE CHOIX HEADER
+session_start();
+if ($_SESSION['autorisation']=='oui') {
+  switch($_SESSION['acces']){
+    case 0:
+      require('includes/header_connexion.php');
+      break;
+    case 1:
+      require('includes/header_enseignant.php');
+      break;
+    case 2:
+      require('includes/header.php');
+      break;
+    case 3:
+      require('includes/header.php');
+      break;
+  }
+}
+else {
+  require('includes/header.php');
+}
+require_once('config/configdb.php'); //configuration bdd */
+?>
+
 <?php require_once('../includes/header_enseignant.php'); ?>
 
 <main>
@@ -10,7 +34,7 @@
       <input type="int" name="nb_grp" placeholder="nb_grp" class="input" required>
       <button type="add" name="ajout">Créer</button>
     </fieldset>
-  </form>
+</form>
   <?php
     if(isset($erreur)) {
        echo '<font color="red">'.$erreur."</font>";
