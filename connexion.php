e790899a92ef054924517a8878f2c01d3b7a89ae
Vaillant <victor.vaillant@student.junia.com>
2021-01-27 10:10:25 +0100

    ajout login admin/user

--- connexion.php
+++ connexion.php
@@ -5,17 +5,44 @@ if ($_SESSION['autorisation']=='oui') {
   exit;
 }*/
 require('includes/header.php');
+require('config/configdb.php');
+session_start();
+
+$conn = mysqli_connect("localhost","root","","projet_cnb3_tpisa");
+
+if (isset($_POST['username'])){
+	$username = stripslashes($_REQUEST['username']);
+	$username = mysqli_real_escape_string($conn, $username);
+	$_SESSION['username'] = $username;
+	$password = stripslashes($_REQUEST['password']);
+	$password = mysqli_real_escape_string($conn, $password);
+    $query = "SELECT * FROM `compte` WHERE login='$username' and mdp='".hash('sha256', $password)."'";
+	$result = mysqli_query($conn,$query) or die(mysql_error());
+	
+	if (mysqli_num_rows($result) == 1) {
+		$user = mysqli_fetch_assoc($result);
+		// vérifier si l'utilisateur est un administrateur ou un utilisateur
+		if ($user['statut'] == 0) {
+			header('location: pages_enseignant/enseignant.php');		  
+		}else{
+			header('location: pages_etudiant/etudiant.php');
+		}
+	}else{
+		$message = "Le nom d'utilisateur ou le mot de passe est incorrect.";
+	}
+}
 ?>
-<main>
-    <form method="POST" action="config/config_co.php">
-        <fieldset>
-          <h3 class="titreformulaire">Connexion</h3>
-          <input type="text" name="login" placeholder="Login" required>
-          <input type="password" minlength="8" name="mdp" placeholder="Mot de passe" required>
-          <input type="hidden" name="affichage" value="">
-          <button type="submit" name="valider">Se connecter</button>
-        </fieldset>
-    </form>
-</main>
+<form class="box" action="" method="post" name="login">
+<h1 class="box-title">Connexion</h1>
+<input type="text" class="box-input" name="username" placeholder="Nom d'utilisateur">
+<input type="password" class="box-input" name="password" placeholder="Mot de passe">
+<input type="submit" value="Connexion " name="submit" class="box-button">
+<p class="box-register">Vous êtes nouveau ici? <a href="register.php">S'inscrire</a></p>
+<?php if (! empty($message)) { ?>
+    <p class="errorMessage"><?php echo $message; ?></p>
+<?php } ?>
+</form>
+</body>
+</html>
 
 <?php require('includes/footer.php'); ?>
