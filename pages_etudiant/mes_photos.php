4b4df7b892571d404c42259ce9f344ad5df5fca7
charlotte <balzac.charlotte@gmail.com>
2021-01-27 07:59:23 +0100

    modif tableau JS

--- pages_etudiant/mes_photos.php
+++ pages_etudiant/mes_photos.php
@@ -6,7 +6,7 @@
 <main class = "container">
 <ul id="photo-list">
     <li class="element">
-      <img src="../img/chat2.jpg" id="1" height="150" width="150">  
+      <img src="../img/chat2.jpg" id="1" height="150" width="150">
     </li>
     <li class="element">
       <img src="../img/chat7.jpg" id="2" height="150" width="150">
@@ -28,7 +28,7 @@
 
 <form method="post" action="supprimer.php"> <!-- get c'est quand on passe par l'URL, or ce n'est pas ce qu'on veut donc on passe par post-->
     <button type="submit" class="btn_supr">Supprimer</button>
-    <input type="hidden" id="jsonId" name="id" value=''/>
+    <input type="hidden" id="jsonId" name="id" value='[]'/>
 </form>
 
 </body>
