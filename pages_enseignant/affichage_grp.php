4629fede0375fb634b36d3a8c9eab489da1279f2
Emmy Vandeville <emmy.v2000@outlook.fr>
2021-01-27 11:57:59 +0100

    Test

--- pages_enseignant/affichage_grp.php
+++ pages_enseignant/affichage_grp.php
@@ -8,25 +8,27 @@ require_once('../config/configdb.php');
 
 // On récuppère les informations du compte utilisateur (compte enseignant)
 $query = $conn->prepare('SELECT * FROM compte WHERE id_compte=:id_compte');
-$query->bindValue(':id_compte', $_SESSION['id_compte'], PDO::PARAM_INT);
+$query->bindValue(':id_compte', $_SESSION['id'], PDO::PARAM_INT);
 $query->execute();
 $compte_enseignant = $query->fetch();
 $query->closeCursor();
 
 // On réccupère les compte élèves de la promo en cours (promo de l'enseignant)
-$pdo = $conn->prepare('SELECT login, mdp FROM compte WHERE promo=:promo AND statut=:staut');
+$statut = 1;
+$pdo = $conn->prepare('SELECT * FROM compte WHERE promo=:promo AND statut=:staut');
 $pdo->bindValue(':promo', $compte_enseignant['promo'], PDO::PARAM_INT);
-$pdo->bindValue(':statut', 1, PDO::PARAM_INT);
+$pdo->bindValue(':statut', $statut, PDO::PARAM_INT);
 $pdo->execute();
-$compte = $pdo->fetchAll();
-$conn=NULL;
+
 ?>
 
 <main>
     <h2 class="titre">Indentifiants élèves promo <?= $compte_enseignant['promo']?> :​</h2>
         <div class="id">
+        <?php while($compte = $pdo->fetchAll()){?>
             <p>Login : <?= $compte['login'] ?></p>
             <p>Mot de passe : <?= $compte['mdp'] ?></p>
+        <?php } ?>
         </div>
 </main>
 
