af1cc3bfc9f7433a748d0a3b72d4580a267f36f7
Louis <louis.houzet@isen.yncrea.fr>
2021-01-28 11:36:50 +0100

    choix promo max pour le telechargement

--- pages_etudiant/telechargement_actuel.php
+++ pages_etudiant/telechargement_actuel.php
@@ -1,5 +1,9 @@
 <?php
 require_once('../config/configdb.php');
+  $statut = $conn->query('SELECT * FROM compte ORDER BY promo DESC LIMIT 1');
+  $statut->execute();
+  $promo = $statut->fetch();
+  $max = $promo['promo'];
 ?>
 
 <table>
@@ -12,13 +16,14 @@ require_once('../config/configdb.php');
   </thead>
   <tbody>
     <?php
-      $stmt = $conn->prepare("SELECT * FROM photo");
+      $stmt = $conn->prepare("SELECT * FROM photo WHERE promo =:act");
+      $stmt->bindValue(':act',$max,PDO::PARAM_INT);
       $stmt->execute();
       while($row = $stmt->fetch()){
     ?>
     <tr>
         <td><?php echo $row['id_photo']?></td>
-        <td>Groupe n°<?php echo $row['team']?></td>
+        <td>Groupe n°<?php echo $row['promo']?></td>
         <td><a href="../config/download_actuel.php?id_photo=<?php echo $row['id_photo']?>">Télécharger</td>
     </tr>
     <?php
