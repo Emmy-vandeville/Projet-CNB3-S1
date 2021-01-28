3581e753017ca9d28346e8c5cbcc3eb51d835688
Maël <lemairemael212@gmail.com>
2021-01-28 09:08:25 +0100

    Photos par promo

--- photos.php
+++ photos.php
@@ -1,4 +1,4 @@
-<?php
+<?php/*
 session_start();
 // On gère le header en fonction du statut
 if ($_SESSION['autorisation']=='oui') {
@@ -13,4 +13,39 @@ if ($_SESSION['autorisation']=='oui') {
 }
 else {
   require('includes/header_connexion.php');;
-}?>
+}*/?>
+
+<?php
+  require_once('config/configdb.php');
+  $categories=['Larves','Vers annélides','Mollusques','Arachnides','Crustacés','Myriapodes','Chenilles','Collemboles','Orthoptères','Diptères','Lépidoptères','Nevroptères','Hymenoptères','Homoptères','Hémiptères','Coléoptères'];
+  $stat = 0;
+  /*$statut = $conn->query('SELECT * FROM compte WHERE statut =:zero');
+  $statut->bindValue(':zero', $stat, PDO::PARAM_INT);*/
+  $statut = $conn->query('SELECT * FROM compte WHERE statut = 0');
+  $statut->execute();
+  $promo = $statut->fetch();
+  $max =  $promo['promo'];
+?>
+<main>
+
+  <section id="accordeon">
+    <?php for($i = 55; $i<=$max; $i++){?>
+      <article>
+        <h2><a class="toggleDetail" href="#">Promo <?= $i ?></a></h2>
+        <div class="panel">
+          <?php
+          $photos = $conn->prepare('SELECT source, promo, id_categorie FROM photo WHERE promo =:act ORDER BY id_photo DESC');
+          $photos->bindValue(':act', $i, PDO::PARAM_INT);
+          $photos->execute();
+          $data = $photos->fetchAll();
+          foreach($data as $key):
+          ?>
+          <img src=<?=$key['source']?> alt="" style="width:10%">
+          <p>Catégorie : <?= $categories[$key['id_categorie']-1]?></p>
+          <?php endforeach; ?>
+        </div>
+      </article>
+    <?php } ?>
+  </section>
+
+</main>
