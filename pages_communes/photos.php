6c9f7912b398a2c37d6979f388eed2d18a278891
Emmy Vandeville <emmy.v2000@outlook.fr>
2021-01-28 21:33:07 +0100

    CSV et les photos apparaissent aussi

--- pages_communes/photos.php
+++ pages_communes/photos.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 // On gère le header en fonction du statut
-/*if ($_SESSION['autorisation']=='oui') {
+if ($_SESSION['autorisation']=='oui') {
   switch($_SESSION['acces']){
     case 0:
       require('../includes/header_enseignant.php');
@@ -13,7 +13,7 @@ session_start();
 }
 else {
   require('../includes/header_connexion.php');;
-}*/?>
+}?>
 
 <?php
   require_once('../config/configdb.php');
@@ -23,13 +23,10 @@ else {
   $max =  $promo['promo'];
 ?>
 <main>
-
   <section id="accordeon">
     <?php for($i = $max; $i>=54; $i--){?>
       <article>
         <h2><a class="toggleDetail" href="#">Promo <?= $i ?></a></h2>
-
-        <a class="export color-nav" href="../csv/promo_<?=$i?>.csv">Exporter les noms</a>
         <?php
         // On crée le tableau
         $tableau=[];
@@ -37,7 +34,6 @@ else {
         $tableau[] = array('Nom du groupe', 'Type', 'Nom latin');
         $tableau[] = array('', '', '');
         ?>
-
         <div class="panel">
           <?php
           $photos = $conn->prepare('SELECT * FROM photo WHERE promo =:act ORDER BY team DESC');
@@ -64,10 +60,15 @@ else {
 
           endforeach; 
           ?>
+
+          <!-- Lien de téléchargement du csv -->
+          <a class="export color-nav" href="../csv/promo_<?=$i?>.csv">Exporter les noms</a>
+
         </div>
       </article>
     <?php } ?>
   </section>
+
   <?php 
   // On ouvre le fichier csv
   $fichier = fopen('../csv/promo_'.$i.'.csv', 'w+');
@@ -77,6 +78,9 @@ else {
   }
   fclose($fichier);
   ?>
+
   <script src="../includes/jquery-3.5.1.min.js"></script>
   <script src='../includes/js_photo_promo.js'></script>
 </main>
+
+<?php require('../includes/footer.php'); ?>
