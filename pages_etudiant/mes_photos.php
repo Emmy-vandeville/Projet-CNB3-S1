8e5814928ab542359af9e4857d9586d823b01fc4
Louis <louis.houzet@isen.yncrea.fr>
2021-01-28 15:00:08 +0100

    MAJ ajout_photo et mes_photo
    ajout d'un choix d'espèce

--- pages_etudiant/mes_photos.php
+++ pages_etudiant/mes_photos.php
@@ -29,6 +29,31 @@ else {
      <input type="hidden" name="MAX_FILE_SIZE" value="1000000000">
      <label>Fichier :</label>
      <input type="file" name="avatar">
+
+     <label for="species-select">Choisissez une espèce:</label>
+
+     <select name="animaux" >
+        <option value="">--Please choose an option--</option>
+        <option value="1" >Larves</option>
+        <option value="2" >Vers annélides</option>
+        <option value="3" >Mollusques</option>
+        <option value="4" >Arachnides</option>
+        <option value="5" >Crustacés</option>
+        <option value="6" >Myriapodes</option>
+        <option value="7" >Chenilles</option>
+        <option value="8" >Collenboles</option>
+        <option value="9" >Orthoptères</option>
+        <option value="10">Diptères</option>
+        <option value="11">Lépidoptères</option>
+        <option value="12">Nevroptères</option>
+        <option value="13">Hymenoptères</option>
+        <option value="14">Homoptères</option>
+        <option value="15">Hémiptères</option>
+        <option value="16">Coléoptères</option>
+
+      </select>
+
+
      <button type="submit" name="envoyer" value="Envoyer le fichier" class="btn-env">Envoyer le fichier</button>
    </fieldset>
  </form>
