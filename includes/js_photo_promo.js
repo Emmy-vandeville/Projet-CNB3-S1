7dc36eb453ddf691b6ee2fb3dbf5bc53690815d1
Maël <lemairemael212@gmail.com>
2021-01-28 15:19:46 +0100

    maj js

--- includes/js_photo_promo.js
+++ includes/js_photo_promo.js
@@ -1,17 +1,20 @@
-$(function(){
-    // cacher tous les élements qui ont la classe panel
-    let allPanels = $(".panel").hide();
-    // On déplie le 1er élement de classe panel
-    $(".panel:first").show();
-    // actions au click sur le lien du h2
-    $(".toggleDetail").click(function(){
-        // si on ne clique pas sur l'element déjà déplié
-        if ($(this).parent().next().is(':hidden')){
-            // on replie les elemnts de classe panel
-            allPanels.slideUp();
-            //On déplie l'element cliqué
-            $(this).parent().next().slideDown();
-        }
-    });
-
+$(function () {
+  //on cache tous les éléments
+  $('.panel').hide();
+  //au click sur h2 on réalise la suite de commandes suivantes
+  $('article h2').click(function(){
+    //si le detail du h2 cliqué est caché alors :
+    if($(this).next('.panel').is(':hidden')){
+      //on fait apparaitre l'article
+      $(this).next('.panel').slideDown();
+      //on change l'icone
+      //$(this).children('i').removeClass('fa-caret-down').addClass('fa-caret-up');
+    }
+    //sinon s'il est affiché on le cache
+    else{
+      $(this).next('.panel').slideUp();
+      //et on change d'icone
+      //$(this).children('i').removeClass('fa-caret-up').addClass('fa-caret-down');
+    }
+  })
 });
